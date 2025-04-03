3353addc5d8fd8234abf86212f1c9d4317e3c7e7
elrampa92 <119736137+elrampa92@users.noreply.github.com>
2025-04-03 09:29:35 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -36,13 +36,13 @@ with col1:
 
     st.write(f"Quota iniziale: {quota_iniziale}")
     st.write(f"Quota disponibile: {quota_disponibile}")
-    st.write(f"Percentuale disponibile: {percentuale_disponibile:.2f}%")
+    st.write(f"Percentuale: {percentuale_disponibile:.2f}%")
 
 # Colonna destra: grafico a torta
 with col2:
     # Creazione del grafico a torta
     fig1, ax1 = plt.subplots()
-    ax1.pie([quota_disponibile, spesa], labels=["Disponibile", "Spesa"], colors=["green", "red"], autopct="%1.1f%%", startangle=90)
+    ax1.pie([quota_disponibile, spesa], labels=["Disponibile", "Spesa"], colors=["blue", "orange"], autopct="%1.1f%%", startangle=90)
     ax1.axis("equal")  # Assicura che la torta sia disegnata come un cerchio.
 
     st.pyplot(fig1)
